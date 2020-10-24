0f36eb716f93ea3f546c80dc3249720a917265cc
Maggie Yang <maggieyang036@gmail.com>
2020-10-24 14:21:52 -0400

    added hardhat test images Added background subtractor to PositionTracker.py

--- PositionTracker.py
+++ PositionTracker.py
@@ -1,1 +1,17 @@
+import cv2
+import numpy
+
+img1 = cv2.imread('background_image.jpg', cv2.IMREAD_UNCHANGED)
+first_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+first_gray = cv2.GaussianBlur(first_gray, (5, 5), 0)
+img2 = cv2.imread('hardhat_photo1.jpg', cv2.IMREAD_UNCHANGED)
+finalImg = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+finalImg = cv2.GaussianBlur(finalImg, (5, 5), 0)
+
+difference = cv2.absdiff(first_gray, finalImg)
+
+cv2.imshow('Background', first_gray)
+cv2.imshow('Original', finalImg)
+cv2.imshow('Difference', difference)
+cv2.waitKey(0)
 
