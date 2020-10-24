75a07c03e9fc7ae330b6e9fe387c9e76e4184922
j52mao <j52mao@uwaterloo.ca>
2020-10-24 12:47:18 -0700

    Corrected the input pins

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -6,17 +6,18 @@ const int rightMotorPin = 5;
 const int botMotorPin = 6;
 const int leftMotorPin = 9;
 
-//Ultrasonic Sensor Input Pins from Rasp Pi Setup
-const int rightUltrasonicPin = 7;
-const int botUltrasonicPin = 8;
-
-int rightDistance;
-int botDistance;
+//Motor Signal Input Pins from Rasp Pi Setup
+const int pinX = 7;
+const int pinZ = 8;
 
 Servo topMotor;
 Servo rightMotor;
 Servo botMotor;
 Servo leftMotor;
+
+int power = 50; //50% power
+int signalX;
+int signalZ;
 
 void setup() {
   Serial.begin(9600);
@@ -26,30 +27,40 @@ void setup() {
   pinMode(rightMotorPin,OUTPUT);
   pinMode(botMotorPin,OUTPUT);
   pinMode(leftMotorPin,OUTPUT);
-  
   topMotor.attach(topMotorPin);
   rightMotor.attach(rightMotorPin);
   botMotor.attach(botMotorPin);
   leftMotor.attach(leftMotorPin);
 
-  //Sensors 
-  pinMode(rightUltrasonicPin, INPUT);
-  pinMode(botUltrasonicPin, INPUT);
+  //Inputs
+  pinMode(pinX, INPUT);
+  pinMode(pinZ, INPUT);
 }
 
 void loop() {
-  //Receiving ultrasonic sensor data from Raspberry Pi
-  rightDistance = digitalRead(rightUltrasonicPin);
-  botDistance = digitalRead(botUltrasonicPin);
+  //Receiving motor signals from Raspberry Pi
+  signalX = digitalRead(pinX);
+  signalZ = digitalRead(pinZ);
+
+  //Applying power to motors
+  moveX(signalX, power);
+  moveZ(signalZ, power);
 }
+
 
 //Functions-------------------------------------------------------------
-int moveVertical(int value){
-  rightMotor.write(map(-value,-100,100,1000,2000));
-  leftMotor.write(map(-value,-100,100,1000,2000));
+int moveX(int signal, int power){
+  if(signal == 0){
+    power = 0;
+  }
+  topMotor.write(map(power,-100,100,1000,2000));
+  botMotor.write(map(-power,-100,100,1000,2000));
 }
 
-int moveHorizontal(int value){
-  topMotor.write(map(value,-100,100,1000,2000));
-  botMotor.write(map(-value,-100,100,1000,2000));
+int moveZ(int signal, int power){
+  if(signal == 0){
+    power = 0;
+  }
+  rightMotor.write(map(-power,-100,100,1000,2000));
+  leftMotor.write(map(-power,-100,100,1000,2000));
 }
