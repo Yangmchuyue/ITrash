3752ee56619ee24264784e4ec24ccf360de5b8a1
Tony Sun <sunwuyue@live.com>
2020-10-28 12:16:19 -0700

    added direction output

--- MotorController.py
+++ MotorController.py
@@ -1,36 +1,89 @@
 import time
 import RPi.GPIO as GPIO
 
-MOTOR1 = 11
-MOTOR2 = 12
-GPIO.setup(MOTOR1, GPIO.OUT) #motor1 arduino
-#GPIO.setup(XX, GPIO.OUT)
-GPIO.setup(MOTOR2, GPIO.OUT) #motor2 arduino
-#GPIO.setup(XX, GPIO.OUT)
-US_TRIGR = 13
-US_ECHO = 14
-GPIO.setup(US_TRIGR, GPIO.OUT)
-GPIO.setup(US_ECHO, GPIO.IN)
+# set up
+motorX = 11
+motorZ = 12
+directionX = 21 #forward and backwards
+directionZ = 22
+GPIO.setup(motorX, GPIO.OUT) #motor1 arduino
+GPIO.setup(motorZ, GPIO.OUT) #motor2 arduino
+GPIO.setup(directionX, GPIO.OUT) #motor1 arduino
+GPIO.setup(directionZ, GPIO.OUT) #motor2 arduino
 
+trigrR = 13
+echoR = 14
+GPIO.setup(trigrR, GPIO.OUT)
+GPIO.setup(echoR, GPIO.IN)
 
+trigrB = 13
+echoB = 14
+GPIO.setup(trigrB, GPIO.OUT)
+GPIO.setup(echoB, GPIO.IN)
 
-def ultrasonic1():
-    GPIO.output(US_TRIGR, False)
-    GPIO.output(US_TRIGR, True)
-    time.sleep(0.00001)
-    GPIO.output(US_TRIGR, False)
+# functions
+def ultrasonic():
+    global distanceR, distanceB
+# right
     pulse_start = 0
     pulse_end = 0
-    while GPIO.input(US_ECHO) == 0:
+    GPIO.output(trigrR, False)
+    GPIO.output(trigrR, True)
+    time.sleep(0.00001)
+    GPIO.output(trigrR, False)
+    while GPIO.input(echoR) == 0:
         pulse_start = time.time()
-    while GPIO.input(US_ECHO) == 1:
+    while GPIO.input(echoR) == 1:
         pulse_end = time.time()
     pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
-    print("distance:", distance, "cm")
+    distanceR = round(pulse_duration * 17150, 2)
+# bottom
+    pulse_start = 0
+    pulse_end = 0
+    GPIO.output(trigrB, False)
+    GPIO.output(trigrB, True)
+    time.sleep(0.00001)
+    GPIO.output(trigrR, False)
+    while GPIO.input(echoB) == 0:
+        pulse_start = time.time()
+    while GPIO.input(echoB) == 1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distanceB = round(pulse_duration * 17150, 2)
 
-while True: ########  X TO BE GIVEN BY CAMERA PI.... NEED FUNCTION TO FETCH THIS VALUE
-    if ultrasonic1() > x:
-        GPIO.output(MOTOR1, GPIO.HIGH)
+    print("distance:", distanceR, "cm")
+    print("distance:", distanceB, "cm")
+
+while True: #NOTE:  X and Z TO BE GIVEN BY CAMERA PI.... NEED FUNCTION TO FETCH THIS VALUE
+    ultrasonic()
+    x = 0
+    z = 0
+    if distanceR > x:
+        GPIO.output(directionX, GPIO.HIGH)
+        GPIO.output(motorX, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(motorX, GPIO.LOW)
+
+    if distanceR < x:
+        GPIO.output(directionX, GPIO.LOW)
+        GPIO.output(motorX, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(motorX, GPIO.LOW)
+
+    if distanceB > z:
+        GPIO.output(directionZ, GPIO.HIGH)
+        GPIO.output(motorZ, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(motorZ, GPIO.LOW)
+
+    if distanceB < z:
+        GPIO.output(directionZ, GPIO.LOW)
+        GPIO.output(motorZ, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(motorZ, GPIO.LOW)
+
+    if abs(distanceR - x) < 1 and abs(distanceB - z):
+        GPIO.output(motorX, GPIO.LOW)
+        GPIO.output(motorZ, GPIO.LOW)
+        break
 
