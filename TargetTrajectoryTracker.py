2ffed04bb988d8e76985e52d87ff3f177ad851cd
Ke Xu <k92xu@uwaterloo.ca>
2020-10-31 19:07:50 -0400

    Update TargetTrajectoryTracker.py

--- TargetTrajectoryTracker.py
+++ TargetTrajectoryTracker.py
@@ -5,7 +5,7 @@ def predictCoord(point0,point1,timeDiff):
     yVel = (point1[1]-point0[1])/timeDiff
     # zVel = (point1[2]-point0[2])/timeDiff
     g = -9.81
-    time = (yVel + (yVel*yVel + 2 * g * point0[1]) ** 0.5)/g
+    time = (-yVel - (yVel*yVel - 2 * g * point0[1]) ** 0.5)/g
     xCoord = point0[0] + xVel * time
     yCoord = 0.0
     # zCoord = point0[2] + zVel * time
@@ -13,5 +13,5 @@ def predictCoord(point0,point1,timeDiff):
     print(xCoord)
     print(yCoord)
 
-    Coord = arr.array('d',[xCoord,yCoord,0.0])
+    Coord = arr.array('d',[xCoord.real,yCoord.real,0.0])
     return Coord
