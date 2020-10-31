8eb0c656a324e6d2064de4de6e0263b8fad95ff7
Joon Kang <joonkang940@hotmail.com>
2020-10-31 13:19:06 -0700

    apply mask

--- CameraController.py
+++ CameraController.py
@@ -1,8 +1,11 @@
 import cv2
+import TargetTrajectoryTracker
 
 # Intialize Video Capture
-#vs = cv2.VideoCapture(0)
-vs = cv2.VideoCapture("hardhatVid.MOV")
+vs = cv2.VideoCapture(0)
+#vs = cv2.VideoCapture("hardhatVid.MOV")
+
+subtractor = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=100, detectShadows=False )
 
 while(True):
     # Get current frame
@@ -11,8 +14,15 @@ while(True):
     # Grayscale the image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Resize the image
+    resize = cv2.resize(gray, (500, 500))
+    
+    # Apply mask onto the image
+    mask = subtractor.apply(resize)
+    
     # Display the final image
-    cv2.imshow('frame', gray)
+    cv2.imshow('Mask', mask)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
