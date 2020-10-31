43d073fdb18e93c5c999648ba16717cd11920123
Joon Kang <joonkang940@hotmail.com>
2020-10-31 14:53:02 -0700

    predict trajectory not done

--- CameraController.py
+++ CameraController.py
@@ -2,17 +2,82 @@ import cv2
 import TargetTrajectoryTracker
 import imutils
 import numpy
+import time
  
+def getNextFrame(centerOne):
+    print("Sleep Start")
+    time.sleep(0.5)
+    print("Sleep End")
+    
+    # Get current frame
+    ret, frame = vs.read()
+    
+    # Resize the image
+    frame = cv2.resize(frame, (500, 500))
+
+    # Grayscale the image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Resize the image
+    resize = cv2.resize(gray, (500, 500))
+    
+    # Apply mask onto the image
+    mask = subtractor.apply(resize)
+    
+    #============================================================
+    # Source: https://www.pyimagesearch.com/2015/09/21/opencv-track-object-movement/
+    # Using the mask found from the previous step, we use the
+    # algorithm below to find and circle the contours.
+    #============================================================
+
+   
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    centerTwo = None
+   
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        if M["m00"]!=0:
+            centerTwo = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, centerTwo, 5, (0, 0, 255), -1)
+    #============================================================
+    print("test3")
+
+    coord = TargetTrajectoryTracker.predictCoord((centerOne[0], centerOne[1], 0), (centerTwo[0], centerTwo[1], 0), 0.5)
+    
+    print("test5")
+    cv2.circle(frame, (int(coord[0]), int(coord[1])), 5, (0, 0, 255), -1)
+    cv2.imshow("FrameTrajectoryPredict", frame)
+    time.sleep(5)
+    print("test6")
+
+       
 # Intialize Video Capture
 vs = cv2.VideoCapture(0)
 #vs = cv2.VideoCapture("hardhatVid.MOV")
 
 subtractor = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=100, detectShadows=False )
+ret, frame = vs.read()
 
 while(True):
     # Get current frame
     ret, frame = vs.read()
     
+    # Resize the image
     frame = cv2.resize(frame, (500, 500))
 
     # Grayscale the image
@@ -55,8 +120,12 @@ while(True):
             cv2.circle(frame, (int(x), int(y)), int(radius),
                 (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            print("hello")
+#            getNextFrame(center)
     #============================================================
-
+    
+    
+    
     # Display the final image
     cv2.imshow("Frame", frame)
 
@@ -66,3 +135,7 @@ while(True):
 # Release the capture
 vs.release()
 cv2.destroyAllWindows()
+
+
+    
+
--- TargetTrajectoryTracker.py
+++ TargetTrajectoryTracker.py
@@ -1,13 +1,14 @@
 import array as arr
 
 def predictCoord(point0,point1,timeDiff):
-    xVel = (point1.x-point0.x)/timeDiff
-    yVel = (point1.y-point0.y)/timeDiff
-    zVel = (point1.z-point0.z)/timeDiff
+    print("test4")
+    xVel = (point1[0]-point0[0])/timeDiff
+    yVel = (point1[1]-point0[0])/timeDiff
+    zVel = (point1[2]-point0[0])/timeDiff
     g = -9.81
-    time = (yVel + (yVel ** 2 + 2 * g * point0.y) ** 0.5)/g
-    xCoord = point0.x + xVel * time
+    time = (yVel + (yVel ** 2 + 2 * g * point0[1]) ** 0.5)/g
+    xCoord = point0[0] + xVel * time
     yCoord = 0.0
-    zCoord = point0.z + zVel * time
+    zCoord = point0[2] + zVel * time
     Coord = arr.array('d',[xCoord,yCoord,zCoord])
     return Coord
