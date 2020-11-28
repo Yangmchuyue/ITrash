49db2fa46cb0dda8ab076c73ff8aca322015446f
Tony Sun <sunwuyue@live.com>
2020-11-28 11:51:03 -0800

    PiToArduino - 24/Nov

--- PiToArduino.py
+++ PiToArduino.py
@@ -9,14 +9,16 @@ import select
 def arduinoToPi():
     # a = 0   # leave here for use in MainPi.py
     line = ser.readline().decode('utf-8').rstrip()
-    print(line)
-    # time.sleep(a)
+    if line != " " and line != "" and line != "\n":
+        print(line)
 
 
 def piToArduino():
-    input = select.select([sys.stdin], [], [], 0.02)[0]  # waits 0.02 seconds for an input
+    input = select.select([sys.stdin], [], [], 0.02)[
+        0]  # waits 0.02 seconds for an input
     if input:
-        keyboardIn = sys.stdin.readline().rstrip()  # input will be "x z", "q" (quit), or " " (pause/unpause)
+        # input will be "x z", "q" (quit), or " " (pause/unpause)
+        keyboardIn = sys.stdin.readline().rstrip()
         print(keyboardIn)
         keyboardIn += "\n"
         ser.write(keyboardIn.encode('utf-8'))
@@ -26,7 +28,8 @@ def piToArduino():
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.flush()
-
+##
+time.sleep(3)
 while True:
     piToArduino()
     arduinoToPi()
